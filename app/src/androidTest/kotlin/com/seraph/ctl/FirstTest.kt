046d1cba441649291bde064efb2a349024db2faa
Alex Naumov <fireserafim@gmail.com>
2015-01-08 02:41:33 +0300

    Initial commit

---
+++ app/src/androidTest/kotlin/com/seraph/ctl/FirstTest.kt
@@ -0,0 +1,12 @@
+package com.seraph.ctl
+
+import junit.framework.TestCase
+
+/**
+ * CTL
+ * Created by seraph on 08.01.2015 3:03 3:04.
+ */
+public class FirstTest : TestCase() {
+    public fun testNothing() {
+    }
+}
